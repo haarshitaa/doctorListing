34418d0e2698d46368e271cfdf90617cf93c470a
haarshitaa <harshitacodes@gmail.com>
2025-04-25 18:43:27 +0530

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/splitit',
+  base: '/doctorListing',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
